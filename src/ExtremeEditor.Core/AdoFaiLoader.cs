ed48b03a84a15c958a7b2ca63be216d03d471b31
Kinetic Napier <u61819269@gmail.com>
2026-09-17 01:07:04 +0900

    Load SetSpeed angle offsets

--- src/ExtremeEditor.Core/AdoFaiLoader.cs
+++ src/ExtremeEditor.Core/AdoFaiLoader.cs
@@ -112,7 +112,8 @@ public static class AdoFaiLoader
                     HitSound = ReadLooseString(action, "hitsound"),
                     HitSoundVolumePercent = ReadLooseDoubleProperty(action, "hitsoundVolume"),
                     GameSound = ReadLooseString(action, "gameSound"),
-                    Planets = ReadLooseString(action, "planets")
+                    Planets = ReadLooseString(action, "planets"),
+                    AngleOffset = ReadLooseDoubleProperty(action, "angleOffset")
                 });
             }
         }
