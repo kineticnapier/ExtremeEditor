65f14ad1ecd7ec2b196a17ed7f6a4a069fb71753
Kinetic Napier <u61819269@gmail.com>
2026-09-19 08:11:27 +0900

    fix: avoid unsupported WPF key gestures

--- src/ExtremeEditor.Wpf/EditorCommands.cs
+++ src/ExtremeEditor.Wpf/EditorCommands.cs
@@ -4,14 +4,14 @@ namespace ExtremeEditor.Wpf;
 
 public static class EditorCommands
 {
-    public static RoutedUICommand Open { get; } = Create("Open", "Open", new KeyGesture(Key.O, ModifierKeys.Control));
-    public static RoutedUICommand Frame { get; } = Create("Frame", "Frame", new KeyGesture(Key.F));
-    public static RoutedUICommand PlayPause { get; } = Create("Play / Pause", "PlayPause", new KeyGesture(Key.Space));
-    public static RoutedUICommand Stop { get; } = Create("Stop", "Stop", new KeyGesture(Key.Escape));
-    public static RoutedUICommand SaveAs { get; } = Create("Save As", "SaveAs", new KeyGesture(Key.S, ModifierKeys.Control));
-    public static RoutedUICommand RotateLeft { get; } = Create("Rotate Left", "RotateLeft", new KeyGesture(Key.OemOpenBrackets));
-    public static RoutedUICommand RotateRight { get; } = Create("Rotate Right", "RotateRight", new KeyGesture(Key.OemCloseBrackets));
+    public static RoutedUICommand Open { get; } = Create("Open", "Open");
+    public static RoutedUICommand Frame { get; } = Create("Frame", "Frame");
+    public static RoutedUICommand PlayPause { get; } = Create("Play / Pause", "PlayPause");
+    public static RoutedUICommand Stop { get; } = Create("Stop", "Stop");
+    public static RoutedUICommand SaveAs { get; } = Create("Save As", "SaveAs");
+    public static RoutedUICommand RotateLeft { get; } = Create("Rotate Left", "RotateLeft");
+    public static RoutedUICommand RotateRight { get; } = Create("Rotate Right", "RotateRight");
 
-    private static RoutedUICommand Create(string text, string name, InputGesture gesture) =>
-        new(text, name, typeof(EditorCommands), new InputGestureCollection { gesture });
+    private static RoutedUICommand Create(string text, string name) =>
+        new(text, name, typeof(EditorCommands));
 }
