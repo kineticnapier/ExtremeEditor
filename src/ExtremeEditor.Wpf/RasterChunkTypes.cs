cf8d99aee74d0c1e64bd0fd260f6be1c1915d16a
Kinetic Napier <u61819269@gmail.com>
2026-09-20 01:37:44 +0900

    feat: add raster chunk request type

---
+++ src/ExtremeEditor.Wpf/RasterChunkTypes.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+using System.Windows;
+
+namespace ExtremeEditor.Wpf;
+
+internal sealed record RasterChunkRequest(
+    RasterChunkKey Key,
+    Rect WorldRect,
+    Rect ScreenRect,
+    int PixelWidth,
+    int PixelHeight,
+    float Zoom,
+    int[] CandidateFloorsDescending,
+    Vector2[] Positions,
+    double[] Angles);
