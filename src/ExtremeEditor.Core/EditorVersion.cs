ea198a51084c01826f15c46f98a37a110929805a
Kinetic Napier <u61819269@gmail.com>
2026-09-20 15:10:31 +0900

    Bump editor version for native floor scene

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.127-prototype";
+    public const string Current = "0.0.128-prototype";
 }
