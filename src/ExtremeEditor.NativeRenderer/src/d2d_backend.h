cca51be7484d616654ff6f54f8757ef716b03d1f
Kinetic Napier <u61819269@gmail.com>
2026-09-20 14:50:56 +0900

    feat: add native Direct2D backend

---
+++ src/ExtremeEditor.NativeRenderer/src/d2d_backend.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <windows.h>
+#include <d2d1_1.h>
+#include <d3d11.h>
+#include <dxgi1_2.h>
+#include <wrl/client.h>
+
+#include <cstdint>
+
+namespace ee
+{
+class D2DBackend
+{
+public:
+    D2DBackend() = default;
+    D2DBackend(const D2DBackend&) = delete;
+    D2DBackend& operator=(const D2DBackend&) = delete;
+    ~D2DBackend();
+
+    bool Initialize(HWND hwnd, std::uint32_t width, std::uint32_t height) noexcept;
+    void Shutdown() noexcept;
+    bool Resize(std::uint32_t width, std::uint32_t height) noexcept;
+    HRESULT RenderFrame(double seconds) noexcept;
+
+private:
+    bool CreateDeviceResources(HWND hwnd, std::uint32_t width, std::uint32_t height) noexcept;
+    bool CreateTargetBitmap() noexcept;
+    void ReleaseTargetBitmap() noexcept;
+
+    std::uint32_t width_ = 1;
+    std::uint32_t height_ = 1;
+
+    Microsoft::WRL::ComPtr<ID3D11Device> d3d_device_;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d_context_;
+    Microsoft::WRL::ComPtr<IDXGISwapChain1> swap_chain_;
+
+    Microsoft::WRL::ComPtr<ID2D1Factory1> d2d_factory_;
+    Microsoft::WRL::ComPtr<ID2D1Device> d2d_device_;
+    Microsoft::WRL::ComPtr<ID2D1DeviceContext> d2d_context_;
+    Microsoft::WRL::ComPtr<ID2D1Bitmap1> target_bitmap_;
+    Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> grid_brush_;
+    Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> accent_brush_;
+    Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> border_brush_;
+};
+}
