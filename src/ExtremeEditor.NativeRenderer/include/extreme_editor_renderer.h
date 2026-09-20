796e2a1cfff215ae6bbaa56ac06b4e672b7d89f1
Kinetic Napier <u61819269@gmail.com>
2026-09-20 14:44:43 +0900

    feat: expose native renderer window lifecycle ABI

--- src/ExtremeEditor.NativeRenderer/include/extreme_editor_renderer.h
+++ src/ExtremeEditor.NativeRenderer/include/extreme_editor_renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <windows.h>
 
 #if defined(_WIN32)
     #if defined(EE_RENDERER_BUILD)
@@ -17,6 +18,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef void* EeRendererHandle;
 
 typedef enum EeResult
 {
@@ -36,8 +39,23 @@ typedef struct EeAbiInfo
     uint32_t diagnostics_size;
 } EeAbiInfo;
 
+typedef struct EeRendererCreateInfo
+{
+    uint32_t struct_size;
+    uint32_t width;
+    uint32_t height;
+    uint32_t flags;
+} EeRendererCreateInfo;
+
 EE_RENDERER_API uint32_t ee_renderer_get_api_version(void);
 EE_RENDERER_API EeResult ee_renderer_get_abi_info(EeAbiInfo* info);
+EE_RENDERER_API EeResult ee_renderer_create(
+    HWND parent,
+    const EeRendererCreateInfo* info,
+    EeRendererHandle* out_renderer);
+EE_RENDERER_API void ee_renderer_destroy(EeRendererHandle renderer);
+EE_RENDERER_API HWND ee_renderer_get_child_hwnd(EeRendererHandle renderer);
+EE_RENDERER_API void ee_renderer_resize(EeRendererHandle renderer, uint32_t width, uint32_t height);
 
 #ifdef __cplusplus
 }
