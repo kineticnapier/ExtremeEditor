5cf73378c857fb9f30b589972511cef12ee68ef3
Kinetic Napier <u61819269@gmail.com>
2026-09-20 15:07:18 +0900

    Add floor scene rendering to Direct2D backend

--- src/ExtremeEditor.NativeRenderer/src/d2d_backend.h
+++ src/ExtremeEditor.NativeRenderer/src/d2d_backend.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "level_scene.h"
+
 #include <windows.h>
 #include <d2d1_1.h>
 #include <d3d11.h>
@@ -7,6 +9,8 @@
 #include <wrl/client.h>
 
 #include <cstdint>
+#include <limits>
+#include <vector>
 
 namespace ee
 {
@@ -21,12 +25,20 @@ public:
     bool Initialize(HWND hwnd, std::uint32_t width, std::uint32_t height) noexcept;
     void Shutdown() noexcept;
     bool Resize(std::uint32_t width, std::uint32_t height) noexcept;
-    HRESULT RenderFrame(double seconds) noexcept;
+    HRESULT RenderFrame(
+        double seconds,
+        const LevelScene* scene,
+        std::uint64_t scene_version,
+        float camera_x,
+        float camera_y,
+        float zoom) noexcept;
 
 private:
     bool CreateDeviceResources(HWND hwnd, std::uint32_t width, std::uint32_t height) noexcept;
     bool CreateTargetBitmap() noexcept;
     void ReleaseTargetBitmap() noexcept;
+    bool SyncSceneGeometry(const LevelScene* scene, std::uint64_t scene_version) noexcept;
+    void DrawScene(const LevelScene& scene, float camera_x, float camera_y, float zoom) noexcept;
 
     std::uint32_t width_ = 1;
     std::uint32_t height_ = 1;
@@ -42,5 +54,11 @@ private:
     Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> grid_brush_;
     Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> accent_brush_;
     Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> border_brush_;
+    Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> floor_brush_;
+    Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> floor_edge_brush_;
+
+    std::vector<Microsoft::WRL::ComPtr<ID2D1PathGeometry>> floor_geometries_;
+    std::vector<std::uint32_t> visible_candidates_;
+    std::uint64_t cached_scene_version_ = std::numeric_limits<std::uint64_t>::max();
 };
 }
