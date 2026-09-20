932f8b40898d19ccdc2978e392502a0d00ddb517
Kinetic Napier <u61819269@gmail.com>
2026-09-20 14:53:05 +0900

    chore: bump editor version for native Direct2D preview

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.125-prototype";
+    public const string Current = "0.0.126-prototype";
 }
