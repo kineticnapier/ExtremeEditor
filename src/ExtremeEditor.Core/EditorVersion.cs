f1e02d515bb8e26e532f568bf4365b50364e22e2
Kinetic Napier <u61819269@gmail.com>
2026-09-20 22:15:39 +0900

    Bump prototype version for audio limiter and native diagnostics

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.131-prototype";
+    public const string Current = "0.0.132-prototype";
 }
