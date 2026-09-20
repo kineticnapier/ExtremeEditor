46bad08e92e8373cc4572e44c0e8311d630e8873
Kinetic Napier <u61819269@gmail.com>
2026-09-20 15:09:40 +0900

    Connect native viewport to level snapshots

--- src/ExtremeEditor.Wpf/Native/NativeLevelViewport.cs
+++ src/ExtremeEditor.Wpf/Native/NativeLevelViewport.cs
@@ -1,12 +1,16 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
+using ExtremeEditor.Core;
 
 namespace ExtremeEditor.Wpf.Native;
 
 public sealed class NativeLevelViewport : HwndHost
 {
     private NativeRendererSession? _session;
+    private LevelDocument? _level;
+    private NativeLevelSnapshot? _snapshot;
+    private bool _frameAllPending;
 
     public NativeLevelViewport()
     {
@@ -14,11 +18,37 @@ public sealed class NativeLevelViewport : HwndHost
         SizeChanged += (_, _) => ResizeNativeChild();
     }
 
+    public void SetLevel(LevelDocument level)
+    {
+        ArgumentNullException.ThrowIfNull(level);
+        _level = level;
+        _snapshot = null;
+        UploadPendingLevel();
+    }
+
+    public void FrameAll()
+    {
+        if (_session is null)
+        {
+            _frameAllPending = true;
+            return;
+        }
+
+        _session.FrameAll();
+        _frameAllPending = false;
+    }
+
     protected override HandleRef BuildWindowCore(HandleRef hwndParent)
     {
         uint width = ToPixelExtent(ActualWidth);
         uint height = ToPixelExtent(ActualHeight);
         _session = NativeRendererSession.Create(hwndParent.Handle, width, height);
+        UploadPendingLevel();
+        if (_frameAllPending)
+        {
+            _session.FrameAll();
+            _frameAllPending = false;
+        }
         return new HandleRef(this, _session.ChildHwnd);
     }
 
@@ -26,6 +56,15 @@ public sealed class NativeLevelViewport : HwndHost
     {
         _session?.Dispose();
         _session = null;
+    }
+
+    private void UploadPendingLevel()
+    {
+        if (_session is null || _level is null)
+            return;
+
+        _snapshot ??= NativeLevelSnapshotBuilder.Build(_level);
+        _session.SetLevel(_snapshot);
     }
 
     private void ResizeNativeChild()
