7f0cc6ff672e28c5967fe6592b3d515292f8593d
Kinetic Napier <u61819269@gmail.com>
2026-09-20 15:20:34 +0900

    Add native viewport camera input selection and icon state

--- src/ExtremeEditor.NativeRenderer/src/renderer.h
+++ src/ExtremeEditor.NativeRenderer/src/renderer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "icon_assets.h"
 #include "level_scene.h"
 #include "native_window.h"
 
@@ -9,6 +10,7 @@
 #include <cstdint>
 #include <memory>
 #include <mutex>
+#include <string>
 #include <thread>
 
 namespace ee
@@ -25,12 +27,17 @@ public:
     void Resize(std::uint32_t width, std::uint32_t height) noexcept;
     bool SetLevel(std::shared_ptr<LevelScene> scene) noexcept;
     void FrameAll() noexcept;
+    void ClearIconAssets() noexcept;
+    bool SetIconAsset(std::uint32_t icon_id, const wchar_t* image_path, const wchar_t* outline_path) noexcept;
 
     [[nodiscard]] HWND ChildHwnd() const noexcept { return window_.Handle(); }
+    [[nodiscard]] std::int32_t SelectedFloor() const noexcept;
+    LRESULT HandleWindowMessage(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) noexcept;
 
 private:
     void RenderLoop() noexcept;
     void StopRenderThread() noexcept;
+    void SelectFloorAt(int screen_x, int screen_y) noexcept;
 
     NativeWindow window_;
     std::thread render_thread_;
@@ -44,11 +51,18 @@ private:
     bool initialize_complete_ = false;
     bool initialize_success_ = false;
 
-    std::mutex scene_mutex_;
+    mutable std::mutex scene_mutex_;
     std::shared_ptr<LevelScene> scene_;
+    std::shared_ptr<const IconAssetTable> icon_assets_ = std::make_shared<IconAssetTable>();
     float camera_x_ = 0.0f;
     float camera_y_ = 0.0f;
     float zoom_ = 28.0f;
+    std::int32_t selected_floor_ = -1;
     std::uint64_t scene_version_ = 0;
+    std::uint64_t icon_assets_version_ = 0;
+
+    bool panning_ = false;
+    UINT pan_button_ = 0;
+    POINT last_mouse_{};
 };
 }
