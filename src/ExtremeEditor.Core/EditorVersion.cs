f4bdcecedd7564fada4fc1692e228e59efcafa79
Kinetic Napier <u61819269@gmail.com>
2026-09-20 23:45:33 +0900

    Bump prototype version for icon instancing

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.134-prototype";
+    public const string Current = "0.0.135-prototype";
 }
