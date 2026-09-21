b2b0847a1aaf123b299c559242eeda0e94306599
Kinetic Napier <u61819269@gmail.com>
2026-09-21 13:03:30 +0900

    Bump version for finalized ADOFAI key chord semantics

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.151-prototype";
+    public const string Current = "0.0.152-prototype";
 }
