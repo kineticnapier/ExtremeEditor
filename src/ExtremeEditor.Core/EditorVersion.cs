2217db023fddc4bd42bcd8cab80feff36598f76c
Kinetic Napier <u61819269@gmail.com>
2026-09-21 12:24:30 +0900

    Bump version for native editor HUD

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.148-prototype";
+    public const string Current = "0.0.149-prototype";
 }
