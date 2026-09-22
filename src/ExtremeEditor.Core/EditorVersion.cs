be2dc73b13408b78b9d6c7e88822f38eb43eb09f
Kinetic Napier <u61819269@gmail.com>
2026-09-22 23:30:03 +0900

    Bump version for smooth Player camera pivot fix

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.208-prototype";
+    public const string Current = "0.0.209-prototype";
 }
