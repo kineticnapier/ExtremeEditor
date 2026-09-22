c08344222e4720590c10193c7a0298bee95619c9
Kinetic Napier <u61819269@gmail.com>
2026-09-23 01:26:18 +0900

    Bump editor version for track runtime active-set optimization

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.224-prototype";
+    public const string Current = "0.0.225-prototype";
 }
