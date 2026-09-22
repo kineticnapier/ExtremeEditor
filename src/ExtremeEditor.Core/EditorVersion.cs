4b22f394262a3402a046b4003d293a0e95b923da
Kinetic Napier <u61819269@gmail.com>
2026-09-22 23:47:40 +0900

    Bump editor version for timeline grouping optimization

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.212-prototype";
+    public const string Current = "0.0.213-prototype";
 }
