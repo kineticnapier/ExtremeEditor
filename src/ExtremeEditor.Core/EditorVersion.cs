5c7165acf71c70a0f2ef7d10579e53de85d43c61
Kinetic Napier <u61819269@gmail.com>
2026-09-24 00:41:25 +0900

    chore: bump version for hitsound extraction RED

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.272-prototype";
+    public const string Current = "0.0.273-prototype";
 }
