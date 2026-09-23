46c45fcf840d8391ecef95e23bab46a09fdfb7ef
Kinetic Napier <u61819269@gmail.com>
2026-09-23 14:17:27 +0900

    chore: bump prototype version for progressive audio batch RED

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.235-prototype";
+    public const string Current = "0.0.236-prototype";
 }
