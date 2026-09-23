7d9453205fccbf4c598f804bfe448de8c1c5da2a
Kinetic Napier <u61819269@gmail.com>
2026-09-23 19:05:30 +0900

    chore: bump version for dirty-open GREEN

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.255-prototype";
+    public const string Current = "0.0.256-prototype";
 }
