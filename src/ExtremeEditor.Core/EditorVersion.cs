2d0fa138e53d48b82e9eb5762a03d7c9f42083ef
Kinetic Napier <u61819269@gmail.com>
2026-09-24 01:15:18 +0900

    chore: bump version for glow extraction fix

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.274-prototype";
+    public const string Current = "0.0.275-prototype";
 }
