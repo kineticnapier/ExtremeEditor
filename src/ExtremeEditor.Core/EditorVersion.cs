faa51ab920e6aff161656fc5135c30645814223c
Kinetic Napier <u61819269@gmail.com>
2026-09-23 23:58:15 +0900

    chore: bump version for texture package fix

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.269-prototype";
+    public const string Current = "0.0.270-prototype";
 }
