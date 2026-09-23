ad312c980dc7872b687b7c5337e49ab318ee47bb
Kinetic Napier <u61819269@gmail.com>
2026-09-23 23:29:43 +0900

    chore: bump version for asset extractor RED

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.267-prototype";
+    public const string Current = "0.0.268-prototype";
 }
