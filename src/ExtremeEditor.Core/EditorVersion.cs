92098a53274235430a1bdcd576376246c0b6379c
Kinetic Napier <u61819269@gmail.com>
2026-09-23 14:46:55 +0900

    Bump prototype version for progressive audio cache integration

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.236-prototype";
+    public const string Current = "0.0.237-prototype";
 }
