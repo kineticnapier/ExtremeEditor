1564b99e3be3eb65bd93ef7ca9871ff772da810a
Kinetic Napier <u61819269@gmail.com>
2026-09-23 19:09:35 +0900

    feat: pass first CLI file to WPF window

--- src/ExtremeEditor.Wpf/App.xaml.cs
+++ src/ExtremeEditor.Wpf/App.xaml.cs
@@ -4,4 +4,14 @@ namespace ExtremeEditor.Wpf;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var window = new MainWindow();
+        MainWindow = window;
+        if (e.Args.Length > 0)
+            window.ScheduleInitialOpen(e.Args[0]);
+        window.Show();
+    }
 }
