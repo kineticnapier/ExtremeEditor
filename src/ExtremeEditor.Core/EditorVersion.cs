a1d3c73b70da1e93415154f620ed7b37d0d3471d
Kinetic Napier <u61819269@gmail.com>
2026-09-23 13:41:54 +0900

    Bump prototype version for audio regression test fixes

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.228-prototype";
+    public const string Current = "0.0.229-prototype";
 }
