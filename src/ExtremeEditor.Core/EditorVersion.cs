b8f78aa71529fa3e2ea8379c9e4a1ee34d1f2e0e
Kinetic Napier <u61819269@gmail.com>
2026-09-25 00:11:30 +0900

    chore: bump editor version to 0.0.291

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.290-prototype";
+    public const string Current = "0.0.291-prototype";
 }
