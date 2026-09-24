ae08bd05651c4e4214a567725cb7a2c5337906e5
Kinetic Napier <u61819269@gmail.com>
2026-09-25 07:35:57 +0900

    Bump editor version for native follow regression

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.300-prototype";
+    public const string Current = "0.0.301-prototype";
 }
