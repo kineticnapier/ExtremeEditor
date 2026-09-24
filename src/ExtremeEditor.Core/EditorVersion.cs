776f6bb8243c42ec667941398f729898bc7557e8
Kinetic Napier <u61819269@gmail.com>
2026-09-24 22:33:14 +0900

    chore: bump version for WPF asset setup RED

--- src/ExtremeEditor.Core/EditorVersion.cs
+++ src/ExtremeEditor.Core/EditorVersion.cs
@@ -2,5 +2,5 @@ namespace ExtremeEditor.Core;
 
 public static class EditorVersion
 {
-    public const string Current = "0.0.282-prototype";
+    public const string Current = "0.0.283-prototype";
 }
