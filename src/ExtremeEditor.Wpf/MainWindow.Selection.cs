28d9d9141ecd19231f6a3e4c7a7ebe25fd900d17
Kinetic Napier <u61819269@gmail.com>
2026-09-25 00:50:34 +0900

    Own shared editor selection state in MainWindow

---
+++ src/ExtremeEditor.Wpf/MainWindow.Selection.cs
@@ -0,0 +1,26 @@
+namespace ExtremeEditor.Wpf;
+
+public partial class MainWindow
+{
+    private readonly EditorSelectionState _selection = new(0);
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        int[] previousSelection = Viewport.SelectedFloors.ToArray();
+        int previousPrimary = Viewport.SelectedFloor;
+
+        _selection.SetFloorCount(_level?.FloorCount ?? 0);
+        _selection.SetSelection(previousSelection, previousPrimary);
+        Viewport.SelectionState = _selection;
+        _selection.Changed += SharedSelectionChanged;
+
+        NativeViewport.SetSelection(_selection.SelectedFloors, _selection.PrimaryFloor);
+    }
+
+    private void SharedSelectionChanged(object? sender, EventArgs e)
+    {
+        NativeViewport.SetSelection(_selection.SelectedFloors, _selection.PrimaryFloor);
+    }
+}
